0174c3763c630af9c9a4c6090d081d1a8bb3b2b6
ArsenMv <arsenmakkay@gmail.com>
2025-01-10 04:57:18 +0300

    Создал класс Notes, добавил 2 строки: title and content

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,5 +1,10 @@
-public class Notes {
-    public static void main(String[] args) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-    }
+@AllArgsConstructor
+@Data
+
+public class Notes {
+    private String title;
+    private String content;
 }
